4fa5f5ff61b30ad26412c75efb5a668550055936
George Castillo <gcastillo@vizrt.com>
2024-08-02 10:01:02 -0600

    Fixed some syntax and type errors in the baud rate generator

--- src/baseline/rtl/baud_rate_gen.vhd
+++ src/baseline/rtl/baud_rate_gen.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity baud_rate_gen is
     generic (
@@ -17,19 +18,20 @@ end entity baud_rate_gen;
 
 architecture behavioral of baud_rate_gen is
 
-    constant BAUD_DIVISOR   : integer   CLK_FREQ / BAUD_RATE;
+    constant BAUD_DIVISOR   : integer   := CLK_FREQ / BAUD_RATE;
 
-    signal  tick_cnt        : integer;
+    signal  tick_cnt        : unsigned(15 downto 0);
 
 begin
 
     process(clk)
+    begin
         if rising_edge(clk) then
             if (rst = '1') then
                 baud_tick       <= '0';
                 tick_cnt        <= (others=>'0');
             else
-                if ( tick_cnt = (BAUD_DIVISOR srl 1) ) then
+                if ( tick_cnt = (to_unsigned(BAUD_DIVISOR, tick_cnt'length) srl 1) ) then
                     baud_tick       <= not baud_tick;
                     tick_cnt        <= (others=>'0');
                 else
