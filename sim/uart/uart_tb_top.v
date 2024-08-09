b0519df8a3071de1fa644703de220a880e23259b
George Castillo <gmcastil@gmail.com>
2024-08-09 08:04:45 -0600

    Fixed a bug where I was using the falling edge instead of the rising edge of the baud tick to send data bits

--- sim/uart/uart_tb_top.v
+++ sim/uart/uart_tb_top.v
@@ -19,8 +19,11 @@ module uart_tb_top ();
   logic         clk_100m00;
   logic         rst_100m00;
 
-  assign clk_100m00 = sys_clk[0];
-  assign rst_100m00 = sys_rst[0];
+  logic         uart_clk;
+  logic         uart_rst;
+
+  assign uart_clk = sys_clk[0];
+  assign uart_rst = sys_rst[0];
 
   // Establish the 100MHz external oscillator provided by the board
   initial begin
@@ -29,6 +32,26 @@ module uart_tb_top ();
       #5ns;
       clk_ext = ~clk_ext;
     end
+  end
+
+  initial begin
+    // Set the UART inputs to well-defined values (i.e., idle) before the power
+    // on reset is dasserted
+    uart_idle();
+
+    // UART stimulus can start several clocks after reset is deasserted
+    wait(uart_rst == 1'b0);
+    repeat (10) @(posedge uart_clk);
+
+    // Now we can start sending data
+    uart_write_byte(8'h48);
+    uart_write_byte(8'h47);
+
+    // Let the simulation run until the UART is ready for more data
+//    repeat (100) @(posedge uart_clk);
+    wait(uart_wr_ready);
+    $display("UART simulation complete");
+    $finish;
   end
 
   // This can be either the synthesizable clock and reset module or a functional
@@ -50,16 +73,37 @@ module uart_tb_top ();
     .BAUD_RATE        (115200)
   )
   uart_dut (
-    .clk              (clk_100m00),      // in    std_logic;
-    .rst              (rst_100m00),      // in    std_logic;
-    .uart_rd_data     (uart_rd_data),    // out   std_logic_vector(7 downto 0);
-    .uart_rd_valid    (uart_rd_valid),   // out   std_logic;
-    .uart_rd_ready    (uart_rd_ready),   // in    std_logic;
-    .uart_wr_data     (uart_wr_data),    // in    std_logic_vector(7 downto 0);
-    .uart_wr_valid    (uart_wr_valid),   // in    std_logic;
-    .uart_wr_ready    (uart_wr_ready),   // out   std_logic;
-    .uart_rxd         (uart_rxd),        // in    std_logic;
-    .uart_txd         (uart_txd)         // out   std_logic
+    .clk              (uart_clk),       // in    std_logic;
+    .rst              (uart_rst),       // in    std_logic;
+    .uart_rd_data     (uart_rd_data),   // out   std_logic_vector(7 downto 0);
+    .uart_rd_valid    (uart_rd_valid),  // out   std_logic;
+    .uart_rd_ready    (uart_rd_ready),  // in    std_logic;
+    .uart_wr_data     (uart_wr_data),   // in    std_logic_vector(7 downto 0);
+    .uart_wr_valid    (uart_wr_valid),  // in    std_logic;
+    .uart_wr_ready    (uart_wr_ready),  // out   std_logic;
+    .uart_rxd         (uart_rxd),       // in    std_logic;
+    .uart_txd         (uart_txd)        // out   std_logic
   );
+
+  // Set UART inputs to quiescent states
+  function void uart_idle();
+    uart_wr_valid   = 1'b0;
+    uart_wr_data    = 8'h0;
+
+    uart_rd_ready   = 1'b0;
+
+    uart_rxd        = 1'b1;
+  endfunction
+
+  // Write a byte to the UART input
+  task automatic uart_write_byte(
+    input   logic   [7:0]   wr_data
+  );
+    uart_wr_data    = wr_data;
+    uart_wr_valid   = 1'b1;
+    wait(uart_wr_valid && uart_wr_ready);
+    @(posedge uart_clk);
+    uart_wr_valid   = 1'b0;
+  endtask
 
 endmodule
--- src/rtl/baseline/uart_tx.vhd
+++ src/rtl/baseline/uart_tx.vhd
@@ -34,7 +34,7 @@ architecture behavioral of uart_tx is
     -- Number of frames sent since the last reset
     signal  tx_frame_cnt        : unsigned(31 downto 0);
 
-    -- Busy shifting out a frame
+    -- Have stored the byte to send and are busy shifting out a frame
     signal  tx_busy             : std_logic;
     -- Shift register is finished with a frame
     signal  tx_done             : std_logic;
@@ -44,8 +44,6 @@ architecture behavioral of uart_tx is
     signal  baud_tick_red       : std_logic;
 
 begin
-
-    uart_txd        <= tx_data_sr(0);
 
     -- Need a rising edge detector for the signal from the baud rate generator
     process(clk)
@@ -58,7 +56,7 @@ begin
             else
                 baud_tick_q     <= baud_tick;
                 baud_tick_qq    <= baud_tick_q;
-                if (baud_tick_q = '0' and baud_tick_qq = '1') then
+                if (baud_tick_q = '1' and baud_tick_qq = '0') then
                     baud_tick_red       <= '1';
                 else
                     baud_tick_red       <= '0';
@@ -71,6 +69,9 @@ begin
     begin
         if rising_edge(clk) then
             if (rst = '1') then
+                -- This should initialize to 1 and if we reset the module, needs to
+                -- be reset to the same value, or we'll send junk to the receiver
+                uart_txd        <= '1';
 
                 tx_busy         <= '0';
                 tx_done         <= '0';
@@ -82,33 +83,36 @@ begin
 
             else
                 if (tx_busy = '0') then
+                    -- Store data when it's available
                     if (uart_wr_valid = '1' and uart_wr_ready = '1') then
                         tx_busy         <= '1';
                         uart_wr_ready   <= '0';
 
-                        -- Add start and stop bits to the data to write
+                        -- Add start and stop bits to the data byte to write and store
+                        -- in the shift register so we can start writing later
                         tx_data_sr      <= TX_STOP_BIT & uart_wr_data & TX_START_BIT;
-                        tx_bit_cnt      <= to_unsigned(TX_FRAME_LEN, tx_bit_cnt'length);
+                        tx_bit_cnt      <= to_unsigned((TX_FRAME_LEN - 1), tx_bit_cnt'length);
                     else
                         uart_wr_ready   <= '1';
                     end if;
+
                 else
+                    -- Now, on the baud tick, shift out one bit until done
                     if (baud_tick_red = '1') then
-                        tx_data_sr(8 downto 0)      <= tx_data_sr(9 downto 1);
                         if (tx_done = '1') then
-                            tx_done         <= '0';
-                            tx_busy         <= '0';
-                            uart_wr_ready   <= '1';
-
+                            tx_busy                     <= '0';
+                            tx_done                     <= '0';
+                            uart_wr_ready               <= '1';
+                            tx_frame_cnt                <= tx_frame_cnt + 1;
                         else
-                            tx_busy         <= '1';
-                            uart_wr_ready   <= '0';
+                            tx_data_sr(8 downto 0)      <= tx_data_sr(9 downto 1);
+                            uart_txd                    <= tx_data_sr(0);
 
+                            tx_bit_cnt                  <= tx_bit_cnt - 1;
                             if (tx_bit_cnt = 1) then
-                                tx_done         <= '1';
-                                tx_frame_cnt    <= tx_frame_cnt + 1;
+                                tx_done                 <= '1';
                             else
-                                tx_bit_cnt      <= tx_bit_cnt - 1;
+                                tx_done                 <= '0';
                             end if;
                         end if;
                     end if;
