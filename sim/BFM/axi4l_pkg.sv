071389bfa7c4bf01de34576d98398909a866b693
George Castillo <4260836+gmcastil@users.noreply.github.com>
2024-11-25 07:12:33 -0700

    Added semaphores to each of the read and write tasks for the AXI4-Lite BFM.
    - This shouldn't be a problem, but for non-reentrant tasks, it is good
      practice to use a semaphore to prevent multiple threads from trying to
      read or write to the actual AXI bus
    - Learned a lot this morning about managing shared access to the
      interface

--- sim/BFM/axi4l_pkg.sv
+++ sim/BFM/axi4l_pkg.sv
@@ -11,20 +11,7 @@ package axi4l_pkg;
         RESP_DECERR     = 2'b11     // Decode error
     } axi4l_resp_t;
 
-//    // AXI4-Lite read and write transactions (use widths derived from the interface)
-//    typedef struct {
-//        logic [ADDR_WIDTH-1:0] waddr;
-//        logic [DATA_WIDTH-1:0] wdata;
-//        logic [(DATA_WIDTH/8)-1:0] wstrb;
-//        axi4l_resp_t bresp;
-//    } axi4l_write_t;
-//    
-//    typedef struct {
-//        logic [ADDR_WIDTH-1:0] raddr;
-//        logic [DATA_WIDTH-1:0] rdata;
-//        axi4l_resp_t rresp;
-//    } axi4l_read_t;
-
+    // AXI4-Lite read or write transactions
     class axi4l_txn #(
         parameter   int ADDR_WIDTH  = 32,
         parameter   int DATA_WIDTH  = 32
@@ -36,32 +23,51 @@ package axi4l_pkg;
 
     endclass
 
+    /* AXI4-Lite bus-functional model */
     class m_axi4l_bfm;
 
         virtual axi4l_if vif;
         bit rst_active;
 
+        semaphore sem_wr;
+        semaphore sem_rd;
+
         // Master BFM constructor
         function new(virtual axi4l_if vif);
             this.vif = vif;
             // AXI4-Lite master needs to see a reset before it can service reads or writes
-            this.rst_active = 0;
-            fork
-                monitor_reset();
-            join_none
+            fork monitor_reset(); join_none
+            // Each read and write task gets it own semaphore to lock access
+            // to the read or write portion of the bus
+            sem_wr = new(1);
+            sem_rd = new(1);
         endfunction
 
         // Perform and AXI4-Lite write transaction
-        task write(axi4l_txn txn);
+        task write(ref axi4l_txn wr_txn);
+            static int wr_count = 0;
             // Can check for reset here
+            sem_wr.get();
+            $display("Obtained AXI4-Lite write access");
+            wr_count++;
+            sem_wr.put();
+            $display("Write count: %0d", wr_count);
         endtask
 
-//        // Perform and AXI4-Lite read transaction
-//        task read(ref axi4l_read_t txn);
-//            // Can check for reset here
-//        endtask
+        // Perform and AXI4-Lite read transaction
+        task read(axi4l_txn rd_txn);
+            static int rd_count = 0;
+            // Can check for reset here
+            sem_rd.get();
+            $display("Obtained AXI4-Lite read access");
+            rd_count++;
+            sem_rd.put();
+            $display("Read count: %0d", rd_count);
+        endtask
 
         task monitor_reset();
+            // Start off in an uninitialized state
+            this.rst_active = 0;
             forever begin
                 @(this.vif.cb);
                 if (this.vif.axi4l_arstn == 0) begin
@@ -70,6 +76,7 @@ package axi4l_pkg;
                     this.vif.bready = 1'b0;
                     this.vif.arvalid = 1'b0;
                     this.vif.rready = 1'b0;
+                    // Wait for reset to deassert
                     @(posedge vif.axi4l_arstn);
                     this.rst_active = 0;
                     $display(`ANSI_GREEN, "AXI4-Lite master reset deasserted", `ANSI_RESET);
