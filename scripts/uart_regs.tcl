7f083a377b79a6b857f9b9667b332f7593b9d85a
George Castillo <4260836+gmcastil@users.noreply.github.com>
2024-12-28 09:06:48 -0700

    Tcl hacking on the UART control and status register script

--- scripts/uart_regs.tcl
+++ scripts/uart_regs.tcl
@@ -1,27 +1,80 @@
 # UART register definitions
-set uart_offset 0x80000000
-set uart_control_reg 0x80000004
-set uart_mode_reg 0x80000004
-set uart_scratch_reg 0x80000020
+set uart_offset 0x00000000
+set uart_control_reg 0x00
+set uart_mode_reg 0x004
+set uart_status_reg 0x08
+set uart_scratch_reg 0x24
 
 # Read and write scratch register
-set jtag_axi_core [get_hw_axis -of_objects [current_hw_device]]
+
+# Write a UART register and check the response
+proc uart_write_reg {uart_reg val} {
+    set wr_addr [expr { "${uart_offset}" + "${uart_reg}" }]
+    set wr_addr [format 0x%x "${wr_addr}"]
+    puts "${wr_addr}"
+
+    # Reset the JTAG to AXI core so that it has a well-defined state
+    set jtag_axi_core [get_hw_axis -of_objects [current_hw_device]]
+    reset_hw_axi ${jtag_axi_core}
+
+    # Create a new JTAG to AXI write transaction and run it (we force it to
+    # overwrite any existing ones)
+    create_hw_axi_txn \
+        -force \
+        -type WRITE \
+        -address "${wr_addr}" \
+        -len 1 \
+        -data "${val}" \
+        wr_txn "${jtag_axi_core}"
+    run_hw_axi -quiet [get_hw_axi_txns wr_txn]
+
+    # And then check the result
+    set bresp [get_property STATUS.BRESP  "${jtag_axi_core}"]
+    if {"${bresp}" != "OKAY"} {
+        puts "Error: Write response received ${bresp}"
+    }
+}
+
+    set jtag_axi_core [get_hw_axis -of_objects [current_hw_device]]
+# Reset the JTAG to AXI core so that it has a well-defined state
+reset_hw_axi ${jtag_axi_core}
 
 create_hw_axi_txn \
     -force \
-    -address ${uart_scratch_reg} \
     -type WRITE \
+    -address "${addr}" \
     -len 1 \
     -data 0xdeadbeef \
     wr_txn "${jtag_axi_core}"
+run_hw_axi -quiet [get_hw_axi_txns wr_txn]
 
-run_hw_axi [get_hw_axi_txns wr_txn]
+set bresp [get_property STATUS.BRESP  "${jtag_axi_core}"]
+if {"${bresp}" != "OKAY"} {
+    puts "Error: Write response received ${bresp}"
+}
 
 create_hw_axi_txn \
     -force \
-    -address ${uart_scratch_reg} \
+    -address "${addr}" \
     -type READ \
     rd_txn "${jtag_axi_core}"
 
-run_hw_axi [get_hw_axi_txns rd_txn]
+run_hw_axi -quiet [get_hw_axi_txns rd_txn]
+set rresp [get_property STATUS.RRESP  "${jtag_axi_core}"]
+if {"${rresp}" != "OKAY"} {
+    puts "Error: Read response received ${rresp}"
+}
+
+create_hw_axi_txn \
+    -force \
+    -address [expr "${uart_offset}" + "${uart_mode_reg}"] \
+    -type WRITE \
+    -len 1 \
+    -data 0xffffffff \
+    wr_txn "${jtag_axi_core}"
+run_hw_axi -quiet [get_hw_axi_txns wr_txn]
+set bresp [get_property STATUS.BRESP  "${jtag_axi_core}"]
+if {"${bresp}" != "OKAY"} {
+    puts "Error: Write response received ${bresp}"
+}
 
