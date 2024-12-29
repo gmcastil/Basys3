f3444f08b378904af56e769ec84d7881565534b1
George Castillo <4260836+gmcastil@users.noreply.github.com>
2024-12-29 09:18:37 -0700

    UART config script is going to eventually wind up being a Tcl version of the driver for the actual UART IP.

--- scripts/uart_regs.tcl
+++ scripts/uart_regs.tcl
@@ -5,20 +5,18 @@ set UART_MODE_REG 0x004
 set UART_STATUS_REG 0x08
 set UART_SCRATCH_REG 0x24
 
-# might make sense to dump these out at some poitn
-# list_property [get_hw_axis]
-#AXI_ADDR_WIDTH AXI_DATA_WIDTH AXI_ID_WIDTH BURST_TYPE_FIXED_SUPPORTED BURST_TYPE_INCR_SUPPORTED BURST_TYPE_WRAP_SUPPORTED CELL_NAME CLASS CORE_UUID NAME PROTOCOL READ_QUEUE_LENGTH STATUS.AXI_READ_BUSY STATUS.AXI_READ_DONE STATUS.AXI_WRITE_BUSY STATUS.AXI_WRITE_DONE STATUS.BRESP STATUS.RRESP WRITE_QUEUE_LENGTH
-
+# Dump out the properties of a specific JTAG to AXI core
 proc display_hw_axi_properties {jtag_axi_core} {
+    set properties [list_property "${jtag_axi_core}"]
 
-    set properties [list_property -class hw_axi "${jtag_axi_core}"]
+    puts "== JTAG to AXI Core =="
     foreach prop "${properties}" {
         set value [get_property "${prop}" "${jtag_axi_core}"]
-        puts "${prop} and ${value}"
+        puts [format "%-30s %s" "${prop}" "${value}"] 
     }
 }
 
-# Write a UART register and check the response
+# Write a UART register and check the response. Returns nothing.
 proc uart_write_reg {reg val} {
     global UART_OFFSET
     set wr_addr [expr { "${UART_OFFSET}" + "${reg}" }]
@@ -48,6 +46,7 @@ proc uart_write_reg {reg val} {
     }
 }
 
+# Read a UART register and check the response. Returns 32-bit read value in hex.
 proc uart_read_reg {reg} {
     global UART_OFFSET
     set rd_addr [expr { "${UART_OFFSET}" + "${reg}" }]
@@ -77,6 +76,10 @@ proc uart_read_reg {reg} {
         return 0x[get_property DATA [get_hw_axi_txns rd_txn]]
     }
 }
+
+# Check JTAG to AXI master status before getting started
+set jtag_axi_core [get_hw_axis -of_objects [current_hw_device]]
+display_hw_axi_properties "${jtag_axi_core}"
 
 # Check scratch register
 set wr_data 0x12341234
